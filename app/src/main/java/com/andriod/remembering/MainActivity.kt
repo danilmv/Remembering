db1ac76de24bcd56782ab383c074c117d794099f
danilmv <43599357+danilmv@users.noreply.github.com>
2022-05-14 23:17:12 +0300

    Fragment

--- app/src/main/java/com/andriod/remembering/MainActivity.kt
+++ app/src/main/java/com/andriod/remembering/MainActivity.kt
@@ -2,10 +2,21 @@ package com.andriod.remembering
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.andriod.remembering.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        supportFragmentManager
+            .beginTransaction()
+            .replace(binding.frame.id, ListFragment())
+            .commit()
     }
 }
